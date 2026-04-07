580bcedc6fe68783235f5e9cfcdb46d2aeb1b662
dpb-hydro <daniel.bartley@hotmail.co.uk>
2026-04-07 16:15:04 +0100

    Added basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,21 @@
-using Springy
+using Springies
 using Test
 using Aqua
 
-@testset "Springy.jl" begin
+@testset "Springies.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(Springy)
+        Aqua.test_all(Springies)
     end
-    # Write your tests here.
+
+    @testset "Unforced pendulum decays to zero" begin
+        pendulum = Pendulum1D(m=10.0, c=0.7, L=5.0)
+        tspan = (0.0, 1000.0)
+        u0 = [0.0, 0.5]
+        Nt = 10
+        u_solved = springy_solve(pendulum, tspan, u0, Nt)
+        θ_final = u_solved[end][1]
+        dθ_final = u_solved[end][2]
+        @test abs(θ_final) < 1e-4
+        @test abs(dθ_final) < 1e-4
+    end
 end
